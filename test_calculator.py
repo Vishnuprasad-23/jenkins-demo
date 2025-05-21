acaf7bd204c81a3850f0026ea86779cf35f79e7e
Vishnuprasad-23 <vishnugopishetty23@gmail.com>
2025-05-21 14:43:01 +0530

    Adding test.

---
+++ test_calculator.py
@@ -0,0 +1,10 @@
+from calculator import add, subtract
+
+def test_add():
+    assert add(1,2) == 3
+
+def test_subtract():
+    assert subtract(5,3) == 2
+
+def test_add_fail():
+    assert add(2,6) == 9
